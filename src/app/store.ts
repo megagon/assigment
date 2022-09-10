c9512c726dfeedef040563a375f1a8fd27ae03e0
Alexey Pimenov <aleksei.pimenov@testlio.com>
2022-09-10 16:35:14 +0300

    clean

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import {
   configureStore, ThunkAction, Action, getDefaultMiddleware,
 } from '@reduxjs/toolkit';
-import { counterSliceReducer } from '../features/counter/counterSlice';
 import { launchesReducer } from '../features/launches/launchesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterSliceReducer,
     lauches: launchesReducer,
   },
   middleware: getDefaultMiddleware({ serializableCheck: false }),
